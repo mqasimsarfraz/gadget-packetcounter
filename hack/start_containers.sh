c9bb1d99e03d66d2858f8078c6aa3388bd09d9d1
Qasim Sarfraz <qasimsarfraz@microsoft.com>
2025-09-09 09:44:11 +0200

    reduce the interval

--- hack/start_containers.sh
+++ hack/start_containers.sh
@@ -16,7 +16,12 @@ CONTAINER_PREFIX="dns_test_container"
 # Start containers and keep them running
 echo "Starting $NUM_CONTAINERS containers..."
 for i in $(seq 1 $NUM_CONTAINERS); do
-    docker run -d --name "${CONTAINER_PREFIX}_$i" busybox sleep 300
+    if [ "$i" -eq 1 ]; then
+        # use different sleep interval for the first container to avoid thundering herd
+        docker run --rm -d --name "${CONTAINER_PREFIX}_$i" busybox sh -c 'while true; do nslookup -type=a example.com.; sleep 0.2; done'
+        continue
+    fi
+    docker run --rm -d --name "${CONTAINER_PREFIX}_$i" busybox sh -c 'while true; do nslookup -type=a example.com.; sleep 0.5; done'
 done
 
 echo "Started $NUM_CONTAINERS containers successfully!"
